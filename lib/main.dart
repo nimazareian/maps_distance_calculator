a50918f4f47732b0ff14dd80816235a2d293ec0d
Nima Zareian <nimazareian80@gmail.com>
2020-07-02 14:22:16 -0700

    added marker

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
-import 'api_key.dart'
-void main() => runApp(new MyApp());
+import 'api_key.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new MyHomePage(),
+    return MaterialApp(
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   var points = <LatLng>[
-        new LatLng(35.22, -101.83),
-        new LatLng(32.77, -96.79),
-        new LatLng(29.76, -95.36),
-        new LatLng(29.42, -98.49),
-        new LatLng(35.22, -101.83),
+    LatLng(35.22, -101.83),
+    LatLng(32.77, -96.79),
+    LatLng(29.76, -95.36),
+    LatLng(29.42, -98.49),
+    LatLng(35.22, -101.83),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(title: new Text('Leaflet Maps')),
-        body: new FlutterMap(
-            options: new MapOptions(
-                center: new LatLng(35.22, -101.83), minZoom: 5.0),
-            layers: [
-              new TileLayerOptions(
-                  urlTemplate:
-                      TEMPLATE_URL,
-                  additionalOptions: {
-                    'accessToken':
-                        API_KEY,
-                    'id': 'mapbox.mapbox-streets-v8'
-                  }),
-              new PolylineLayerOptions(
-                polylines: [
-                  new Polyline(
-                    points: points,
-                    strokeWidth: 5.0,
-                    color: Colors.red
-                  )
-                ]
-              )
-            ]));
+    return Scaffold(
+      appBar: AppBar(title: Text('Leaflet Maps')),
+      body: FlutterMap(
+        options: MapOptions(center: LatLng(49.2827, 123.1207), minZoom: 5.0),
+        layers: [
+          TileLayerOptions(
+            urlTemplate: TEMPLATE_URL,
+            additionalOptions: {
+              'accessToken': API_KEY,
+              'id': 'mapbox.mapbox-streets-v8'
+            },
+          ),
+          PolylineLayerOptions(
+            polylines: [
+              Polyline(points: points, strokeWidth: 5.0, color: Colors.red)
+            ],
+          ),
+          MarkerLayerOptions(
+            markers: [
+              Marker(
+                width: 80.0,
+                height: 80.0,
+                point: new LatLng(51.5, -0.09),
+                builder: (ctx) => Container(
+                  child: new FlutterLogo(),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
