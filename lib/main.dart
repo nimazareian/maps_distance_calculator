c41bf2ca62b7bbfe5c4c9a80a4aea440ea15fe67
Nima Zareian <nimazareian80@gmail.com>
2020-07-02 13:57:27 -0700

    Can view MapBox map in app with polylines drawn on it

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-
-
-// import 'package:mapbox_search/mapbox_search.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
-// import 'package:location/location.dart';
-
-        // <meta-data android:name="com.google.android.geo.API_KEY"
-        //        android:value="YOUR KEY HERE"/>
-    // <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
-    
-void main() {
-  runApp(MyApp());
-}
+import 'package:latlong/latlong.dart';
+import 'api_key.dart'
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return new MaterialApp(
+      home: new MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  var points = <LatLng>[
+        new LatLng(35.22, -101.83),
+        new LatLng(32.77, -96.79),
+        new LatLng(29.76, -95.36),
+        new LatLng(29.42, -98.49),
+        new LatLng(35.22, -101.83),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ,
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-
-class testWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-              'You have pushed the button this many times:',
-            );
+    return new Scaffold(
+        appBar: new AppBar(title: new Text('Leaflet Maps')),
+        body: new FlutterMap(
+            options: new MapOptions(
+                center: new LatLng(35.22, -101.83), minZoom: 5.0),
+            layers: [
+              new TileLayerOptions(
+                  urlTemplate:
+                      TEMPLATE_URL,
+                  additionalOptions: {
+                    'accessToken':
+                        API_KEY,
+                    'id': 'mapbox.mapbox-streets-v8'
+                  }),
+              new PolylineLayerOptions(
+                polylines: [
+                  new Polyline(
+                    points: points,
+                    strokeWidth: 5.0,
+                    color: Colors.red
+                  )
+                ]
+              )
+            ]));
   }
 }
