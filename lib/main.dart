fffce1fa43692335e53ef402992d4d6a32a7844b
Nima Zareian <nimazareian80@gmail.com>
2020-07-03 14:21:10 -0700

    Switched package to mapbox_gl and can view map on screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong/latlong.dart';
 import 'api_key.dart';
-import 'package:flutter_map_picker/flutter_map_picker.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
+// import 'package:latlong/';
 
 void main() => runApp(MyApp());
 
@@ -21,69 +20,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  MapController mapController = MapController();
+  @override
+  Widget build(BuildContext context) {
+    return const FullMap();
+  }
+}
 
-  var points = <LatLng>[
-    LatLng(35.22, -101.83),
-    LatLng(32.77, -96.79),
-    LatLng(29.76, -95.36),
-    LatLng(29.42, -98.49),
-    LatLng(35.22, -101.83),
-  ];
+class FullMap extends StatefulWidget {
+  const FullMap();
 
-//   _onMapTapped(LatLng point) {
-//     CustomPoint<num> screenPosition = Epsg3857().latLngToPoint(mapController.center, mapController.zoom);
-//     print('Map Center: ${mapController.center}, zoom: ${mapController.zoom}');
-//     print('Screen position: $screenPosition');
-// }
-  // CustomPoint<num> northWestPoint =
-  //     Epsg3857().latLngToPoint(mapPosition.bounds.northWest, mapPosition.zoom);
-  // CustomPoint<num> markerPoint =
-  //     Epsg3857().latLngToPoint(markerLatLng, mapPosition.zoom);
-  // double x = markerPoint.x - northWestPoint.x;
-  // double y = markerPoint.y - northWestPoint.y;
+  @override
+  State createState() => FullMapState();
+}
+
+class FullMapState extends State<FullMap> {
+  MapboxMapController mapController;
+
+  void _onMapCreated(MapboxMapController controller) {
+    mapController = controller;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Leaflet Maps')),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
-      body: FlutterMap(
-        options: MapOptions(
-          center: LatLng(49.2827, -123.1207),
-          minZoom: 5.0,
-        ),
-        layers: [
-          TileLayerOptions(
-            urlTemplate: TEMPLATE_URL,
-            additionalOptions: {
-              'accessToken': API_KEY,
-              'id': 'mapbox.mapbox-streets-v8'
-            },
-          ),
-          PolylineLayerOptions(
-            polylines: [
-              Polyline(points: points, strokeWidth: 5.0, color: Colors.red)
-            ],
-          ),
-          MarkerLayerOptions(
-            markers: [
-              Marker(
-                width: 80.0,
-                height: 80.0,
-                point: LatLng(49.2827, -123.1207),
-                builder: (ctx) => Container(
-                  child: Icon(Icons.location_on),
-                ),
-              ),
-            ],
-          ),
-        ],
-        mapController: MapController(),
-      ),
-    );
+    return new Scaffold(
+        body: MapboxMap(
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: const CameraPosition(target: LatLng(0.0, 0.0)),
+    ));
   }
 }
